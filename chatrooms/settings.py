4c296c2ce79b43cad0f05212acfc8dd6d4d5ff34
Jpadilla1 <jpadilla1293@gmail.com>
2014-04-29 23:13:50 -0400

    fix heroku database issue

--- chatrooms/settings.py
+++ chatrooms/settings.py
@@ -142,8 +142,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
